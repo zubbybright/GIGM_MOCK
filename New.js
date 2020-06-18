48d5b30e440ad74fbb3df906e6b6cf635b733758
Zubby <zubbybrightson@gmail.com>
2020-06-18 16:10:00 +0100

    GIGM mock page with bootstrap

---
+++ New.js
@@ -0,0 +1,38 @@
+
+/*var num1=  28;
+var num2= 34;
+var num3= 67;
+
+function sumNumbers() {
+	alert(num1 + num2 + num3);
+	console.log(num1 + num2 + num3);
+	
+}
+console.log( sumNumbers());
+
+function prodNumbers() {
+	alert(num1 * num2 * num3);
+	console.log(num1 * num2 * num3);
+	
+}
+console.log( prodNumbers());
+
+function diffNumbers() {
+	alert(num1 - num2 - num3);
+	console.log(num1 - num2 - num3);
+	
+}
+console.log( diffNumbers());*/
+
+alert(" Welcome to 'God Is Good Motors' Official Website");
+
+
+function bookNow(){
+	var Enquire= prompt ("Please Enter your subscription password", "");
+	if(Enquire!="") {
+		alert("Your trip has been successfully booked");
+	}else
+	{
+		alert("Please check your input and try again.");
+	}
+}
